c1c4e8507bc06f47a707345aeea19461efcbec10
701b <701b.vn14a@gmail.com>
2020-08-24 22:05:46 +0900

    modify to show only day weather icon

--- js/script/weather.js
+++ js/script/weather.js
@@ -3,21 +3,11 @@ const weatherIconEl = document.getElementById('weather-icon');
 const temperatureValueEl = document.getElementById('temperature-value');
 const humidityValueEl = document.getElementById('humidity-value');
 const renewingPeriod = 60000;
-var Weather;
-(function (Weather) {
-    Weather[Weather["SUNNY"] = 0] = "SUNNY";
-    Weather[Weather["PARTLY_CLOUDY"] = 1] = "PARTLY_CLOUDY";
-    Weather[Weather["RAINY"] = 2] = "RAINY";
-    Weather[Weather["THUNDERSTORMS"] = 3] = "THUNDERSTORMS";
-    Weather[Weather["SNOWY"] = 4] = "SNOWY";
-    Weather[Weather["CLOUDY"] = 5] = "CLOUDY";
-    Weather[Weather["FOGGY"] = 6] = "FOGGY";
-})(Weather || (Weather = {}));
 function renewWeather() {
     if (weatherIconEl && temperatureValueEl && humidityValueEl) {
         setDataByPostHttpRequest('weather', `cityName=${locationInfo.cityName}`, (data) => {
             const weatherData = JSON.parse(data);
-            weatherIconEl.src = `http://openweathermap.org/img/wn/${weatherData.weather_icon}@2x.png`;
+            weatherIconEl.src = `http://openweathermap.org/img/wn/${weatherData.weather_icon.substring(0, 2) + 'd'}@2x.png`;
             temperatureValueEl.innerText = (weatherData.temperature - 273.15).toFixed(1) + '°';
             humidityValueEl.innerText = weatherData.humidity + '%';
         });
--- ts/script/weather.ts
+++ ts/script/weather.ts
@@ -6,22 +6,12 @@ const humidityValueEl: HTMLElement | null = document.getElementById('humidity-va
 
 const renewingPeriod: number = 60000;
 
-enum Weather {
-    SUNNY,
-    PARTLY_CLOUDY,
-    RAINY,
-    THUNDERSTORMS,
-    SNOWY,
-    CLOUDY,
-    FOGGY
-}
-
 function renewWeather(): void {
     if (weatherIconEl && temperatureValueEl && humidityValueEl) {
         setDataByPostHttpRequest('weather', `cityName=${ locationInfo.cityName }`, (data: string): void => {
             const weatherData: any = JSON.parse(data);
             
-            weatherIconEl.src = `http://openweathermap.org/img/wn/${ weatherData.weather_icon }@2x.png`;
+            weatherIconEl.src = `http://openweathermap.org/img/wn/${ weatherData.weather_icon.substring(0, 2) + 'd' }@2x.png`;
             temperatureValueEl.innerText = (weatherData.temperature - 273.15).toFixed(1) + '°';
             humidityValueEl.innerText = weatherData.humidity + '%';
         });
